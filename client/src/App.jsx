626d81db602a58fee27a7ef22756b113396ab866
Priyanshu7058 <priyanshupatel940@gmail.com>
2025-04-06 02:38:26 +0530

    home css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
@@ -7,24 +7,34 @@ import Login from "./pages/Login";
 import Profile from './pages/Profile';
 import Cart from './pages/Cart';
 
-// Import the AuthProvider
 import { AuthProvider } from './context/AuthContext';
+
+function AppContent() {
+  const location = useLocation();
+
+  const hideNavbarPaths = ['/login']; // Add more if needed
+  const shouldShowNavbar = !hideNavbarPaths.includes(location.pathname);
+
+  return (
+    <>  
+      {shouldShowNavbar && <Navbar />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/search" element={<SearchedProducts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
-    <AuthProvider> {/* Wrap everything inside AuthProvider */}
+    <AuthProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/search" element={<SearchedProducts />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/cart" element={<Cart />} />
-          {/* <Route path="/categories" element={<Categories />} />
-          <Route path="/offers" element={<Offers />} /> */}
-        </Routes>
+        <AppContent />
       </Router>
     </AuthProvider>
   );
